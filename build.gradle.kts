36dbfc4a9bbd01720387175eb92d134822aeeba3
timjuic <tim.juic@gmail.com>
2024-01-09 15:00:59 +0100

    Made app executable with ./gradlew run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
-    id("java")
+    id("application")
+}
+
+application {
+    // Specify the main class for your application
+    mainClass.set("me.timjuice.Main")
 }
 
 group = "me.timjuice"
@@ -11,10 +16,11 @@ repositories {
 
 dependencies {
     implementation ("org.apache.kafka:kafka-clients:3.6.1")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
 //    testImplementation(platform("org.junit:junit-bom:5.9.1"))
 //    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+//tasks.test {
+//    useJUnitPlatform()
+//}
--- src/main/java/me/timjuice/Main.java
+++ src/main/java/me/timjuice/Main.java
@@ -7,7 +7,15 @@ public class Main {
         System.out.println("Hello world!");
 
         Thread temperatureProducerThread = new Thread(new TemperatureSensorProducer());
+        temperatureProducerThread.setDaemon(true); // Set the thread as a daemon
 
         temperatureProducerThread.start();
+
+        // Add a sleep or some other logic to keep the main thread alive
+        try {
+            Thread.sleep(Long.MAX_VALUE); // Sleep for a long time or use some other logic
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
