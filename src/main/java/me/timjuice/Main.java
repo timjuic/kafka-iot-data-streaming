62c56cf73678ab346e5a009bbf29f934c093f713
timjuic <tim.juic@gmail.com>
2024-01-09 14:02:32 +0100

    Add temperature sensor producer

--- src/main/java/me/timjuice/Main.java
+++ src/main/java/me/timjuice/Main.java
@@ -1,7 +1,13 @@
 package me.timjuice;
 
+import me.timjuice.producers.TemperatureSensorProducer;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Thread temperatureProducerThread = new Thread(new TemperatureSensorProducer());
+
+        temperatureProducerThread.start();
     }
 }
